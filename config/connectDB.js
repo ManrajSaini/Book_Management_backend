20a5932c60f1aa1a26521eb02154e2ab693c91fd
Manraj Singh <manraj.saini123@gmail.com>
2023-11-08 10:34:08 +0530

    Server started and database connected

---
+++ config/connectDB.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Hurray! Connected to database");
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/connectDB");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+connectDB();
+
+app.get("/", (req,res) => {
+    return res.send({
+        "success": true,
+        "error_code": null,
+        "message": "Server is Running",
+        "data": "Server is Live 🎉"
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, Hurray!!");
+});
